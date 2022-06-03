890f3c987544688c67c5bdb9f13e56da6d8c0113
mohamad <aghi.mohamad@gmail.com>
2022-06-03 13:54:57 +0300

    completed

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+<h2>About me</h2>
+<p>hello there my name is mohamad aghi and i am a full stack web developer</p>
+<img src={image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,11 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div>
+    <div id="home">
+      <h1 style={{color:"firebrick"}}>your name is {name} from {city}</h1>
+    </div>
+  </div>;
 }
 
 export default Home;
